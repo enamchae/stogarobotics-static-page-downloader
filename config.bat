79e0301619c84d26f0729d9d1ffac7fea110599a
dc <@>
2020-01-15 18:57:10 -0500

    Add config.bat

---
+++ config.bat
@@ -0,0 +1,9 @@
+:: Supply the SSH URL of (a fork of) the target repository.
+set ssh_url=""
+
+:: Supply the Git identification options.
+set email=""
+set name=""
+
+:: Supply a commit message.
+set commitmsg="Add generated static files"
